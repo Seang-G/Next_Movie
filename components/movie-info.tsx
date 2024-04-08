91c2ea23da08af22994115019f7e4f63ba7e8635
Seang-G <mgw7730@gmail.com>
2024-04-08 19:09:15 +0900

    add styles

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,6 +1,8 @@
 import { URL } from "../app/(home)/page";
 
-async function getMovie(id:string) {
+import styles from "../styles/movie-info.module.css"
+
+export async function getMovie(id:string) {
   const response =await fetch(`${URL}/${id}`);
   
   return response.json();
@@ -8,5 +10,15 @@ async function getMovie(id:string) {
 
 export default async function MovieInfo({id}: {id:string}) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>
+  return (
+    <div className={styles.containor}>
+      <img className={styles.poster} src={movie.poster_path} />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐ {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target="_blank">HomePage &rarr;</a>
+      </div>
+    </div>
+  )
 }
